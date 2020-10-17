dbfa8e1dce8507702aceb28b6b4fef4f5e5b9205
Soren Nissen <s.nissen.bass@gmail.com>
2020-10-17 00:39:17 +0000

    added 4 sql files to schema to create DB tables

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,10 @@
--- Drop and recreate Users table (Example)
-
 DROP TABLE IF EXISTS users CASCADE;
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  first_name VARCHAR(255) NOT NULL,
+  surname VARCHAR(255) NOT NULL,
+  phone VARCHAR(255) NOT NULL,
+  is_owner BOOLEAN NOT NULL DEFAULT FALSE,
+  password VARCHAR(255) NOT NULL
 );
