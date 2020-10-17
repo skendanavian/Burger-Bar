c31020dc7c6abfc067399f51bf15f8ae7c7061b2
Dan Pletzke <dpletzke@gmail.com>
2020-10-17 02:12:01 +0000

    add user seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,14 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (first_name, surname, phone, email, password) VALUES
+('Alice', 'Anderson', '800-500-0100', 'dummy@example.com', 'password'),
+('Kevin', 'Kent', '800-500-0200', 'dummy@example.com', 'password'),
+('Greg', 'Gregerson', '800-500-0300', 'dummy@example.com', 'password'),
+('Aaron', 'Carter', '800-500-0400', 'dummy@example.com', 'password'),
+('Kyle', 'Smith', '800-500-0500', 'dummy@example.com', 'password'),
+('Francis', 'Bourgouin', '800-500-0600', 'dummy@example.com', 'password'),
+('Bobby', 'Smith', '800-500-0700', 'dummy@example.com', 'password'),
+('Edward', 'Smith', '800-500-0800', 'dummy@example.com', 'password'),
+('Brian', 'Smith', '800-500-0900', 'dummy@example.com', 'password')
+
+INSERT INTO users (first_name, surname, phone, email, password, is_owner) VALUES
+('Billy', 'Owner', '800-500-1000', 'dummy@example.com', 'password', true);
