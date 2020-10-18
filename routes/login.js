30d69f3c00f4d670d6649f03fda224eb79602464
Soren Nissen <s.nissen.bass@gmail.com>
2020-10-18 01:06:07 +0000

    removed redundant comments

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
-//I added these routes here simply to view the non-index pages while editing.. Not sure where they should go long term and feel free to move them. 
+
 router.get('/', (req, res) => {
   res.render("login");
 });
