2a389f12e00f221586923110178612cbb505272f
Dan Pletzke <dpletzke@gmail.com>
2020-10-19 06:15:52 +0000

    anonymize authtoken and accountsid with .env

--- send_sms.js
+++ send_sms.js
@@ -1,7 +1,7 @@
 // Your Account Sid and Auth Token from twilio.com/console
 // DANGER! This is insecure. See http://twil.io/secure
-const accountSid = 'ACcf8d0b637095949ddb03f816fa61b73e';
-const authToken = 'b440a31ca561193c5ea75ac4de9127fe';
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
 
@@ -10,6 +10,6 @@ client.messages
   .create({
      body: 'Example text!',
      from: '+16503534858',
-     to: 'change me!'
+     to: '+17089274496'
    })
   .then(message => console.log(message.sid));
