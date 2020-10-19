aa9f4f0d38cf6e38d58007047aa2f564ea8e6c8c
Dan Pletzke <dpletzke@gmail.com>
2020-10-19 21:04:49 +0000

    make name, description, picture price from db

--- db/index.js
+++ db/index.js
@@ -5,15 +5,11 @@ const getMenu = function (db) {
   return db.query(`
   SELECT *
   FROM menu_items;
-  `).then(res => {
-    console.log('Im in the helper', res.rows);
-    return res.rows;
-  });
+  `);
 }
 
 
 module.exports = { getMenu };
-// console.log(getMenu());
 
 
 
