d6bd1e4edacac5793d75ae625b40a398b188cb55
Dan Pletzke <dpletzke@gmail.com>
2020-10-19 20:17:39 +0000

    get menu working db helper

--- db/index.js
+++ db/index.js
@@ -1,17 +1,20 @@
 
 
+
 const getMenu = function (db) {
   return db.query(`
   SELECT *
   FROM menu_items;
   `).then(res => {
+    console.log('Im in the helper', res.rows);
     return res.rows;
   });
 }
 
-console.log(getMenu());
 
 module.exports = { getMenu };
+// console.log(getMenu());
+
 
 
 
--- routes/menu.js
+++ routes/menu.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router  = express.Router();
+const { getMenu } = require('../db');
 
 module.exports = (db) => {
 
-router.get('/', (req, res) => {
-  res.render("menu");
-});
+  router.get('/', (req, response) => {
+    getMenu(db).then(res => {
+      response.render("menu");
+    });
+  });
   return router;
 };
