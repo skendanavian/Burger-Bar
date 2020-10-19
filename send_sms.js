63356fef129c4e944b6071189a154d4fc4222e9d
Dan Pletzke <dpletzke@gmail.com>
2020-10-19 15:55:08 +0000

    twilio not recieving api request

--- send_sms.js
+++ send_sms.js
@@ -1,3 +1,8 @@
+// load .env data into process.env
+require('dotenv').config();
+
+console.log(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+
 // Your Account Sid and Auth Token from twilio.com/console
 // DANGER! This is insecure. See http://twil.io/secure
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
@@ -8,8 +13,8 @@ const client = require('twilio')(accountSid, authToken);
 //change the 'to' field to your phone number to test!
 client.messages
   .create({
-     body: 'Example text!',
+     body: "What's going on?",
      from: '+16503534858',
      to: '+17089274496'
    })
-  .then(message => console.log(message.sid));
+  .then(message => console.log(message));
