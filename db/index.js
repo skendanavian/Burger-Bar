9412185c0083cf496db700d8c3c5836ee74d5e07
Dan Pletzke <dpletzke@gmail.com>
2020-10-20 18:28:13 +0000

    showing correct data on kitchen page

--- db/index.js
+++ db/index.js
@@ -10,8 +10,23 @@ const getMenu = function(db) {
 
 const getIncompleteOrders = function (db) {
   return db.query(`
-  SELECT *
-  FROM orders
+  SELECT  orders.id as order_id,
+          orders.created_at,
+          orders.description,
+          menu_items.id AS menu_item_id,
+          menu_items.name AS menu_item_name,
+          order_items.id AS order_item_id,
+          order_items.quantity,
+          users.first_name,
+          users.surname,
+          users.phone
+  FROM order_items
+  JOIN orders
+  ON order_items.order_id = orders.id
+  JOIN menu_items
+  ON order_items.menu_item_id = menu_items.id
+  JOIN users
+  ON orders.user_id = users.id
   WHERE is_complete IS FALSE;
   `);
 }
--- routes/kitchen.js
+++ routes/kitchen.js
@@ -5,9 +5,33 @@ const router  = express.Router();
 module.exports = (db) => {
 
   router.get('/', (req, response) => {
+
     getIncompleteOrders(db).then(res => {
-      const order_items = res.rows;
-      console.log(order_items);
+      const incompleteOrderItems = res.rows;
+      const orders = {};
+
+      incompleteOrderItems.forEach(item => {
+        orders[item.order_id] = {
+          created_at: item.created_at,
+          description: item.description,
+          first_name: item.first_name,
+          surname: item.surname,
+          phone: item.phone,
+          order_items: {}
+        }
+      });
+
+      incompleteOrderItems.forEach(item => {
+        const { order_id, order_item_id } = item;
+        const { order_items } = orders[order_id];
+
+        order_items[order_item_id] = {
+            menu_item_name: item.menu_item_name,
+            quantity: item.quantity
+          }
+        });
+
+
       response.render("kitchen", { orders });
     })
   });
