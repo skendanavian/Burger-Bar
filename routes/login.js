be28de8b01add75d22d3b41869c7efa2450f7334
Dan Pletzke <dpletzke@gmail.com>
2020-10-21 19:39:24 +0000

    require bcrypt on login register

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router  = express.Router();
+const bcrypt = require('bcrypt');
 
 module.exports = (db) => {
 
@@ -8,7 +9,7 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  
+
 });
 
   return router;
