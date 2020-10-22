4a4aa947ff481d4febc4fbe96117287f68397d55
Dan Pletzke <dpletzke@gmail.com>
2020-10-22 17:17:22 +0000

    work on data validating

--- routes/register.js
+++ routes/register.js
@@ -3,23 +3,40 @@ const router  = express.Router();
 const bcrypt = require('bcrypt');
 const { getUserWithEmail, register } = require('../db');
 
+const validatePhone = (phone) => {
+
+}
+
+
+
 
 
 module.exports = (db) => {
 
   router.get('/', (req, res) => {
+    const { userId } =  req.session;
     res.render("register");
   });
 
   router.post('/', (req, response) => {
     const { firstName, lastName, email, phone, password  } = req.body;
+    const { userId } = req.session;
+    const errorMsgs = [];
+
     const hashedPassword = bcrypt.hashSync(password, 10);
 
-    const dataMissing = Object.values(req.body).some(val => !val);
+    const dataMissing = data.some(val => !val);
     if (dataMissing) {
       response.statusCode = 400;
-      response.render("register", { error: 'Fill out the whole thing!' });
     }
+
+
+    if () { //firstName, lastName under 50 characters,
+
+    }
+
+    if ()
+    response.render("register", { error: 'Fill out the whole thing!' });
 
     getUserWithEmail(db, email).then(res => {
 
@@ -28,9 +45,7 @@ module.exports = (db) => {
       response.render("register", { error: 'Account already exists!' });
       } else {
       const data = {firstName, lastName, email, phone, hashedPassword};
-      console.log(data);
       register(db, data).then(res => {
-        console.log(res.rows);
         const { id: user_id } = res.rows[0];
         req.session.user_id = user_id;
         response.redirect('order');
