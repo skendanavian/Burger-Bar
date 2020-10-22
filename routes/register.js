0751a9cd1f5ae8025a3a7bd40f019d1eee8fe277
Dan Pletzke <dpletzke@gmail.com>
2020-10-22 17:52:45 +0000

    implement register validator helper funciton

--- routes/register.js
+++ routes/register.js
@@ -1,14 +1,43 @@
 const express = require('express');
-const router  = express.Router();
 const bcrypt = require('bcrypt');
+const phoneUtil = require('google-libphonenumber');
+const router  = express.Router();
 const { getUserWithEmail, register } = require('../db');
 
-const validatePhone = (phone) => {
 
+const validateRegisterData = (data) => {
+  const { firstName, lastName, email, phone, password  } = data;
+  const errorMsgs = [];
+
+  const dataMissing = Object.values(data).some(val => !val);
+  if (dataMissing) {
+    errorMsgs.push('Please fill out the whole form.');
+  }
+
+  if (firstName.length > 50) {
+    errorMsgs.push('First name entry has too many characters.');
+  }
+
+  if (lastName.length > 50) {
+    errorMsgs.push('Last name entry has too many characters.');
+  }
+
+  const emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  const isValidEmail = emailPattern.test(email);
+  if(!isValidEmail) {
+    errorMsgs.push('Please enter a valid email address.');
+  }
+
+  if (!phoneUtil.isPossibleNumber(phone)) {
+    errorMsgs.push('Please enter a valid phone number.');
+  }
+
+  if(password.length < 6) {
+    errorMsgs.push('Your password is too short! Please enter one at least 6 characters long.');
+  }
+
+  return errorMsgs;
 }
-
-
-
 
 
 module.exports = (db) => {
@@ -20,32 +49,25 @@ module.exports = (db) => {
 
   router.post('/', (req, response) => {
     const { firstName, lastName, email, phone, password  } = req.body;
-    const { userId } = req.session;
-    const errorMsgs = [];
+    const { userId, ownerId } = req.session;
 
-    const hashedPassword = bcrypt.hashSync(password, 10);
-
-    const dataMissing = data.some(val => !val);
-    if (dataMissing) {
+    const errorMsgs = validateRegisterData(req.body);
+    if (errorMsgs.length) {
       response.statusCode = 400;
+      response.render("register", { userId, ownerId, errorMsgs });
     }
-
-
-    if () { //firstName, lastName under 50 characters,
-
-    }
-
-    if ()
-    response.render("register", { error: 'Fill out the whole thing!' });
 
     getUserWithEmail(db, email).then(res => {
 
       if (res !== null) {
-      response.statusCode = 400;
-      response.render("register", { error: 'Account already exists!' });
+        response.statusCode = 400;
+        errorMsgs.push('User with this email already exists.')
+        response.render("register", { userId, ownerId, errorMsgs });
       } else {
-      const data = {firstName, lastName, email, phone, hashedPassword};
-      register(db, data).then(res => {
+
+        const hashedPassword = bcrypt.hashSync(password, 10);
+        const data = {firstName, lastName, email, phone, hashedPassword};
+        register(db, data).then(res => {
         const { id: user_id } = res.rows[0];
         req.session.user_id = user_id;
         response.redirect('order');
